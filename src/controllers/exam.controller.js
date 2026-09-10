c7c0894be003cc75955d6a88422abae93bd27654
Пользователь <muxtarjangabaev07@gmail.com>
2026-09-10 07:52:32 +0500

    fix

--- src/controllers/exam.controller.js
+++ src/controllers/exam.controller.js
@@ -122,6 +122,7 @@ export const checkChannelMember = async (req, res) => {
       "-1003722907958", // ms3
       "-1003923825017", // ms4
       "-1003968513581", // ms5
+      "-1002280402248",
     ];
 
     const results = await Promise.allSettled(
